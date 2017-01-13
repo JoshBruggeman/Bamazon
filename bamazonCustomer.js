918ba0b0e3ee3b725c7bb73235bea4f0a38309cf
Josh Bruggeman <joshrbruggeman@gmail.com>
2017-01-13 15:50:31 -0600

    npm

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -22,4 +22,23 @@ connection.query("SELECT * FROM products", function(err,res){
 	for (var i = 0; i < res.length; i++) {
 		console.log(res[i].id + "|" + res[i].product_name + "|" + res[i].department_name + "|" + res[i].price + "|" + res[i].stock_quantity);
 	}
-});
+}); 
+
+inquirer.prompt([
+  {
+    type: 'list',
+    name: 'theme',
+    message: 'Which product would you like to buy?',
+    choices: [
+      '1',
+      '2',
+      '3',
+      '4',
+      '5',
+      '6',
+      '7',
+      '8',
+      '9',
+      '10'
+      ]
+}]);
